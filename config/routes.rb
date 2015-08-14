b502ae4e67a92a94ac59e9fe815f1f101c9990bd
Rian Bogle <rbogle@usgs.gov>
2015-08-14 11:33:12 -0700

    fixed for via get post rubric

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 if Rails::VERSION::MAJOR >= 3
-  match 'blocks/:action', :controller => 'blocks'
-  match 'time_entries', :controller => 'timelog'
-  match 'time_entries/:action',:controller => 'timelog'
-  match 'time_entries/:action?project_id=:id',:controller => 'timelog'
+  match 'blocks/:action', via: [:get, :post], :controller => 'blocks'
+  match 'time_entries', via: [:get, :post], :controller => 'timelog'
+  match 'time_entries/:action', via: [:get, :post],:controller => 'timelog'
+  match 'time_entries/:action?project_id=:id', via: [:get, :post],:controller => 'timelog'
 else
   ActionController::Routing::Routes.draw do |map|
     map.connect 'blocks/:action', :controller => 'blocks'
